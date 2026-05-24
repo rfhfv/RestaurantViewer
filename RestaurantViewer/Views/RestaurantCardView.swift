f5fa2ea4c1faa0a3641fad496dedc2cc62cd086e
Anna <sozontova.an@icloud.com>
2026-05-24 21:10:45 +0700

    Add RestaurantCardsView

---
+++ RestaurantViewer/Views/RestaurantCardView.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantCardView.swift
+//  RestaurantViewer
+//
+//  Created by admin on 24.05.2026.
+//
+
+import Foundation
---
+++ RestaurantViewer/Views/RestaurantCardsView.swift
@@ -0,0 +1,8 @@
+//
+//  RestaurantCardsView.swift
+//  RestaurantViewer
+//
+//  Created by admin on 24.05.2026.
+//
+
+import Foundation
